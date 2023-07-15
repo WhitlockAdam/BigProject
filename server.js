40ec8344f8539685ead14cc57aeacfbbdb67c4e3
John Slauter <jslauter@knights.ucf.edu>
2023-07-14 22:41:23 -0400

    Initial Commit

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.set("port", (process.env.PORT||5000));
-app.use(cors());
 app.use(bodyParser.json());
+app.use(cors());
+
 
 app.use((req, res, next) =>{
 
